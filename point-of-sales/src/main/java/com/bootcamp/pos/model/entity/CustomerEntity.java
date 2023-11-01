e89bb1c6c6760628cc5d35f0b6b4336d691623da
Diktya19 <diktya@gmail.com>
2023-11-01 11:39:29 +0700

    feat: add customer entity

--- point-of-sales/src/main/java/com/bootcamp/pos/model/entity/CustomerEntity.java
+++ point-of-sales/src/main/java/com/bootcamp/pos/model/entity/CustomerEntity.java
@@ -1,5 +1,18 @@
 package com.bootcamp.pos.model.entity;
 
-public class CustomerEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_customer")
+public class CustomerEntity {
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
 }
